df949cf58e1b2c6cf916acf513a09c0cb95f26e7
Alvaro Aragoneses bl0800 <aj.aragoneses@gmail.com>
2014-10-09 15:00:02 +0200

    CD6: Añadido tercer mensaje

--- src/Practica.java
+++ src/Practica.java
@@ -4,6 +4,7 @@ public class Practica {
 	public static void main(String[] args) {
 		System.out.println("Release 1.0");
 		System.out.println("Hola Mundo!");
+		System.out.println("cerrando!");
 	}
 
 }
